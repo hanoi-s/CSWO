f34493747da85164698dff821a998c6d3879a552
hanoi <shanicka_minor@dlsu.edu.ph>
2022-12-02 23:40:30 +0800

    added comments

--- controller/controller.js
+++ controller/controller.js
@@ -8,13 +8,15 @@ const StatusModel = require('../models/statusModel.js');
 
 const controller = {
     getDashboard: function (req, res) {
+        // Renders dashboard page with all requests from database
+
         db.findRequests(RequestModel, {}, {}, function(result){
-            // console.log(result);
-            res.render('dashboard', {request:result});
+            res.render('dashboard', {request:result});      
         });
     },
 
     getNewOrder: function (req, res) {
+        // Renders new order page with data coming from database such as types, categories, employees, and status
 
         // Query for reading all Types from the Database
         db.findMany(TypeModel, {}, {}, function(typesresult){
@@ -33,6 +35,8 @@ const controller = {
     },
 
     getWorkOrder: function (req, res) {
+        // Renders work order page catering to a specific work order request
+        // also with data coming from database such as types, categories, employees, and status
 
         // Query for reading all Types from the Database
         db.findMany(TypeModel, {}, {}, function(typesresult){
@@ -43,9 +47,6 @@ const controller = {
                     // Query for reading all Statuses from the Database
                     db.findMany(StatusModel, {}, {}, function(statusesresult){
                         db.findOne(RequestModel, {_id: req.params.woid}, {}, function(result){
-                            // // console.log(result.Location) // undefined
-                            // console.log(String(result.Location));
-                            // res.render('workorder', {request:result});
                             res.render('workorder', {request:result, type:typesresult, category:categoriesresult, employee:employeesresult, status:statusesresult});
                         });
                         
@@ -60,6 +61,7 @@ const controller = {
     },
 
     postNewOrder: async function(req, res) {
+        // This is what happens in order to create a single work order
 
         // Generate the timestamp for DateReceived (Timestamp that the Work Order was created)
         var today = new Date();
@@ -77,6 +79,8 @@ const controller = {
         })
     
         // Collects the information from the form
+        // They are separated because they are objects.
+        // We are storing their value to use in finding them on the DB
         var CATEGORY = req.body.workCtg;
         var TYPE = req.body.workType;
         var INCHARGE = req.body.incharge;
@@ -89,11 +93,8 @@ const controller = {
                 EmployeeModel.findOne({ FirstName:INCHARGE}).then((incharge) => {
                     StatusModel.findOne({ StatusName:STATUS}).then((status) => {
                         // console.log(incharge);
-                        // console.log(category);
-                        // console.log(type);
-                        // console.log(status);
 
-                        // Creates a new request instance with all the information collected
+                        // Creates a new Request instance with all the information collected
                         // both from the form and the database
                         const request = new RequestModel({
                             Location: req.body.location,
@@ -103,46 +104,42 @@ const controller = {
                             DateReceived: datetime,
                             DateApproved: null,
                             DateCompleted: null,
-                            Requester: requester,       // This is where new requester instance is stored
+                            Requester: requester,       // This is where new Requester instance is stored
                             Status: status,             // This is where the data from db is stored
                             Category: category,         // This is where the data from db is stored
                             Type: type,                 // This is where the data from db is stored
                             InCharge: incharge,         // This is where the data from db is stored
                         });
 
-                        // request.save() - Saves the new request instance on the database
+                        // request.save() - Saves the new Request instance on the database
                         request.save()
                         .then((result) => {
                             // console.log(result);
-                            
-                            requester.save();               // requester.save() - Saves the new requester information instance on the database
+                            requester.save();               // requester.save() - Saves the new Requester information instance on the database
                             res.redirect('/dashboard');     // Redirects the user on the dashboard to view new work order
-                        })
-                            .catch((err) => {
-                            console.log(err);
-                        })
+                        }).catch((err) => {console.log(err);})
                     })
                 })
             })
         })
-    
-
     },
 
     postUpdateOrder: async function(req, res) {
-        var today = new Date();
-        var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var datetime = date+' '+time;
-
         // Collects the information from the form
+        var DATETARGET = req.body.DateTarget;
         var CATEGORY = req.body.workCtg;
         var TYPE = req.body.workType;
-        var DATETARGET = req.body.DateTarget;
         var INCHARGE = req.body.incharge;
         var STATUS = req.body.status;
 
+        // If the status was changed to "Approved",
+        // timestamp is generated to be stored on the DB
         if (STATUS == "Approved") {
+            // Generate the timestamp for DateApproved
+            var today = new Date();
+            var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var datetime = date+' '+time;
             var DATEAPPROVED = datetime;
         } else {
             var DATEAPPROVED = null;
@@ -154,7 +151,6 @@ const controller = {
             CategoryModel.findOne({ CategoryName:CATEGORY}).then((category) => {
                 EmployeeModel.findOne({ FirstName:INCHARGE}).then((incharge) => {
                     StatusModel.findOne({ StatusName:STATUS}).then((status) => {
-
                         RequestModel.findOne({_id: req.params.woid}, {}, function(request){
                             RequestModel.updateOne({_id: req.params.woid}, 
                                 {$set: {
@@ -164,8 +160,7 @@ const controller = {
                                     Category: category,         // This is where the data from db is stored
                                     Type: type,                 // This is where the data from db is stored
                                     InCharge: incharge,         // This is where the data from db is stored
-                                }},
-                                function(request){
+                                }}, function(request){
                                     RequestModel.findOne({_id: req.params.woid}, {}, function(result2){
                                         res.redirect('/workorder/' + req.params.woid);
                                     });
