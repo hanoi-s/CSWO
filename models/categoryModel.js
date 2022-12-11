db2f8e7c1d50e2a229dec85d897b117dcb25861f
hanoi <shanicka_minor@dlsu.edu.ph>
2022-12-11 16:00:57 +0800

    test

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
     CategoryID: {type: String},
-    CategoryName: {type: String}
+    CategoryName: {type: String},
 })
 
 module.exports = mongoose.connection.model('Category', categorySchema);
